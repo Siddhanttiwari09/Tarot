90ec7d90003fb2c7d429ed558a1837dfab07fb6a
Siddhanttiwari09 <51109612+Siddhanttiwari09@users.noreply.github.com>
2022-03-24 16:08:13 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+     // fader
+     const faders = document.querySelectorAll('.fade-in');
+     const sliders = document.querySelectorAll('.scroll-in');
+ 
+     const appearOptions={
+         threshold: 0,
+         rootMargin: "0px 0px -150px 0px"
+     };
+     
+     const appearOnScroll = new IntersectionObserver
+     (function(
+         entries,
+         appearOnScroll) {
+     entries.forEach(entry => {
+     if (!entry.isIntersecting) {
+       return;
+      } else {
+       entry.target.classList.add("appear");
+       appearOnScroll.unobserve(entry.target);
+         }
+       });
+     },
+     appearOptions);
+ 
+     faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+     });
+ 
+     sliders.forEach (slider => {
+         appearOnScroll.observe(slider);
+     });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing-2", {
+        strings: ["Astrologer", "Numerologer", "Tarot Card Reader" , "Counsellor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
